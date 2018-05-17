d6bb054733004285d1446924d31ed24e40b1d1ca
hxx688 <hxx688@163.com>
2018-05-17 10:48:02 +0800

    Update shanpay.js
    update support get

--- js/page/my/shanpay.js
+++ js/page/my/shanpay.js
@@ -6,9 +6,16 @@ mui.plusReady(function() {
 	var self = plus.webview.currentWebview();
 	var data = self.param;
 	for(var key in data){
+	    if('mobile_param' == key) {
+	        if(data[key] && data[key].indexOf("get") != -1) {
+                document.forms['paysubmit'].method = "GET";
+            }
+        }
+
         if('pay_url'==key){
 //      	mui.toast('pay_url:'+data[key]);
             document.forms['paysubmit'].action = data[key];
+
         }else{
         	var input = document.createElement('input');
 			input.setAttribute('type', 'hidden');
